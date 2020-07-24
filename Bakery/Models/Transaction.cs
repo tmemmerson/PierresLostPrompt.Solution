1aa0f0045161578a0810bb9bef9212e9385a47bd
Tristan Emmerson <tristan@stickerslug.com>
2020-07-24 10:49:01 -0700

    create transaction class model.

--- Bakery/Models/Transaction.cs
+++ Bakery/Models/Transaction.cs
@@ -4,5 +4,34 @@ namespace Bakery.Models
 {
   public class Transaction
   {
+
+    public string Description {get; set;}
+
+    public int Id {get; }
+
+    private static List<Transaction> _instances = new List<Transaction> { };
+
+    public Transaction(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    
+    public static List<Transaction> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Transaction Find(int searchId)
+    {
+      return _instances[searchId -1];
+    }
     
   }
+}
