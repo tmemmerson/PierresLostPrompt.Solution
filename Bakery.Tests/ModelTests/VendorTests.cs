19d29c20c64924af5dccb393456413980ffa469e
Tristan Emmerson <tristan@stickerslug.com>
2020-07-24 16:03:41 -0700

    Red flag VendorConstructor_CreateInstanceOfVendor.

---
+++ Bakery.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.TestTools
+{
+  [TestClass]
+  public class VendorTest : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreateInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("vendor name");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
