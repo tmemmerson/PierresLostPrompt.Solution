a2a539859e835dfe36775404a1e57ff0383f9ba8
Tristan Emmerson <tristan@stickerslug.com>
2020-07-24 11:06:53 -0700

    create transactionscontroller controller.

--- Bakery/Controllers/TransactionsController.cs
+++ Bakery/Controllers/TransactionsController.cs
@@ -7,5 +7,29 @@ namespace Bakery.Controllers
 {
   public class TransactionsController : Controller
   {
-    
+    [HttpsGet("/vendors/{vendorId}/transactions/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpsGet("/vendors/{vendorId}/transactions/{transactionId}")]
+    public ActionResult Show(int vendorId, int transactionId)
+    {
+      Transaction transaction = Transaction.Find(transactionId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("transaction", transaction);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+    [HttpPost("/transactions/delete")]
+    public ActionResult DeleteAll()
+    {
+      Transaction.ClearAll();
+      return View();
+    }
+  }
   }
