f1d144088c1f0077ecd7e2d342422ec8e02c2e78
Tristan Emmerson <tristan@stickerslug.com>
2020-07-24 10:44:59 -0700

    create vendor class model.

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -4,5 +4,34 @@ namespace Bakery.Models
 {
   public class Vendor
   {
-    
+    private static List<Vendor> _instances = new List<Vendor> { };
+
+    public string Name { get; set; }
+
+    public int Id { get; }
+
+    public List<Transaction> Transactions { get; set; }
+
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Transactions = new List<Transaction> { };
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances[searchId -1];
+    }
+
+    public void AddTransaction(Transaction transaction)
+    {
+      Transactions.Add(transaction);
+    }
   }
