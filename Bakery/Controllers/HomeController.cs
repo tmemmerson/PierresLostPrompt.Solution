85fbd90a60a8babc192839c943e75bb7eb7e86f1
Tristan Emmerson <tristan@stickerslug.com>
2020-07-24 10:33:11 -0700

    create HomeController controller.

---
+++ Bakery/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+  public class HomeController : Controller
+  {
+    
+  }
